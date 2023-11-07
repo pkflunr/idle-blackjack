a59a00f1d0672a7cf277ce11c32a2cc58a8daf82
pkflunr <miguel.p.valmonte@gmail.com>
2023-11-07 15:09:32 -0800

    Update deck.gd

--- deck.gd
+++ deck.gd
@@ -6,5 +6,14 @@ func create_deck():
 	deck = []
 	for suit in Card.Suit:
 		for value in range(1,14):
-			deck.append(CardData(suit, value))
+			deck.append(CardData.new(suit, value))
 
+func draw_card() -> CardData:
+	if deck:
+		return deck.pop_back()
+	else:
+		return null
+
+func shuffle_deck():
+	if deck:
+		deck.shuffle()
