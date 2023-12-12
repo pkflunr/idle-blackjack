63808c2dbbe79b71d30f97191d778b792ffc14b3
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-12-12 12:08:00 -0800

    Create upgrade.gd

---
+++ Scripts/upgrade.gd
@@ -0,0 +1,19 @@
+extends Resource
+class_name Upgrade
+
+# temp
+enum UpgradeType {
+	CHANGE_BET_AMOUNT,
+	CHANGE_BET_MULTIPLIER,
+	UPGRADE_IDLE
+}
+
+@export var type:UpgradeType
+
+func do_action():
+	# temp
+	match(type):
+		UpgradeType.CHANGE_BET_AMOUNT:
+			pass
+		UpgradeType.CHANGE_BET_MULTIPLIER:
+			pass
