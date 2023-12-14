e88c07ff1d209cda40c5b75711442bbf5954eaad
jchu62 <89236068+jchu62@users.noreply.github.com>
2023-12-14 08:10:36 -0800

    Add the ability to change texture/descriptions for idle processes

--- Scripts/idle_process.gd
+++ Scripts/idle_process.gd
@@ -7,6 +7,8 @@ class_name IdleProcess
 @export var base_cost:int
 @export var amount:int
 @export var time:float
+@export var description:String
+@export var texture:Texture2D
 
 func get_current_cost():
 	return int(base_cost * pow(1.15, amount))
--- Scripts/idle_process_widget.gd
+++ Scripts/idle_process_widget.gd
@@ -4,7 +4,9 @@ extends PanelContainer
 @onready var count_label = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/CountLabel
 @onready var earnings_label = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer2/EarningsLabel
 @onready var price_label = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/PriceLabel
+@onready var description_label = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/DescriptionLabel
 @onready var buy_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/BuyButton
+@onready var texture_rect = $MarginContainer/VBoxContainer/HBoxContainer/TextureRect
 
 
 var idle_process_name:String
@@ -18,7 +20,9 @@ func _process(delta):
 	count_label.text = "Count: " + str(idle_process.amount)
 	earnings_label.text = "One earns " + str(idle_process.value * idle_process.upgrade_multiplier) + " every " + str(int(idle_process.time)) + " seconds"
 	price_label.text = "$"+str(idle_process.get_current_cost())
+	description_label.text = idle_process.description
 	buy_button.disabled = Stats.cash < idle_process.get_current_cost()
+	texture_rect.texture = idle_process.texture
 
 
 func _on_buy_button_pressed():
