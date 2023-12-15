b1ed83b7a56db85d4764901ba7b9afc0736c1ec8
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-12-14 18:46:06 -0800

    add savegame

--- Scenes/stats.gd
+++ Scenes/stats.gd
@@ -12,13 +12,41 @@ var idle_process_dict:Dictionary
 
 var upgrade_list:UpgradeList = preload("res://Assets/Resources/upgrade_list.tres")
 var upgrade_dict:Dictionary
-func _ready():
+
+var save_location:String = "user://save_data.json"
+
+func new_game():
 	idle_process_dict = {}
 	for idle_process in idle_process_list.list:
 		idle_process_dict[idle_process.name] = idle_process
 	
 	upgrade_dict = {}
 	for upgrade in upgrade_list.list:
+		upgrade_dict[upgrade.name] = upgrade
+
+func save_game():
+	var saved_upgrade_list:UpgradeList = UpgradeList.new()
+	var saved_process_list:IdleProcessList = IdleProcessList.new()
+	saved_upgrade_list.list = []
+	saved_process_list.list = []
+	
+	for key in upgrade_dict:
+		saved_upgrade_list.list.append(upgrade_dict[key])
+	for key in idle_process_dict:
+		saved_process_list.list.append(idle_process_dict[key])
+	
+	ResourceSaver.save(saved_upgrade_list, "user://saved_upgrade_list.tres")
+	ResourceSaver.save(saved_process_list, "user://saved_process_list.tres")
+
+func load_game():
+	var saved_upgrade_list:UpgradeList = load("user://saved_upgrade_list.tres")
+	var saved_process_list:IdleProcessList = load("user://saved_process_list.tres")
+	idle_process_dict = {}
+	for idle_process in saved_process_list.list:
+		idle_process_dict[idle_process.name] = idle_process
+	
+	upgrade_dict = {}
+	for upgrade in saved_upgrade_list.list:
 		upgrade_dict[upgrade.name] = upgrade
 
 var current_bet:int = 100
--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -3,9 +3,11 @@ extends Button
 
 func _on_new_game_pressed():
 	MusicScene.start_game()
+	Stats.new_game()
 	get_tree().change_scene_to_file("res://Scenes/main_screen.tscn")
 
 func _on_load_game_pressed():
 	# do some loading here
 	MusicScene.start_game()
+	Stats.load_game()
 	get_tree().change_scene_to_file("res://Scenes/main_screen.tscn")
